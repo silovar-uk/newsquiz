f18f81752bd88be487cd496dbce9387e6f28a021
silovar-uk <46244124+silovar-uk@users.noreply.github.com>
2026-07-06 21:14:56 +0900

    Use generated News Context Quiz icon in published app

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'news-context-quiz-v2-static';
-const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
+const CACHE_NAME = 'news-context-quiz-v3-icon';
+const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './app-icon.png'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
