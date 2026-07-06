e31fa51044d8eda981c69ae74fabcaed33396ce5
silovar-uk <46244124+silovar-uk@users.noreply.github.com>
2026-07-07 08:27:35 +0900

    Load import and explanation UX overrides

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './uiux-overrides.css';
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
